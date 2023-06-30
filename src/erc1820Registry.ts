e2a6c8cf8b0cf574267c93b59c8d283e06500017
NanezX <victor.nanezj@gmail.com>
2023-06-30 10:37:02 -0400

    extrospection scan only allowed interpreters

--- src/erc1820Registry.ts
+++ src/erc1820Registry.ts
@@ -4,8 +4,9 @@ import { ExpressionDeployer } from "../generated/schema";
 import {
   IERC1820_NAME_IEXPRESSION_DEPLOYER_V1_HASH,
   generateTransaction,
+  ExtrospectionPerNetwork,
 } from "./utils";
-
+import { log } from "@graphprotocol/graph-ts";
 export function handleInterfaceImplementerSet(
   event: InterfaceImplementerSet
 ): void {
@@ -13,14 +14,28 @@ export function handleInterfaceImplementerSet(
     event.params.interfaceHash.toHex() ==
     IERC1820_NAME_IEXPRESSION_DEPLOYER_V1_HASH
   ) {
-    const expressionDeployer = new ExpressionDeployer(
-      event.params.account.toHex()
+    const extrospection = ExtrospectionPerNetwork.get();
+
+    const isAllowedInterpreter =
+      extrospection.scanOnlyAllowedInterpreterEVMOpcodes(event.params.account);
+
+    log.info(
+      `XD_0: Address: ${event.params.account.toHex()} - allowed: ${isAllowedInterpreter}`,
+      []
     );
-    const transaction = generateTransaction(event);
-    expressionDeployer.deployTransaction = transaction.id;
+    // If allowed, then will create the ExpressionDeployer entity.
+    // Consequently, only contracts called from the ExpressionDeployer readed will
+    // be show
+    if (isAllowedInterpreter) {
+      const expressionDeployer = new ExpressionDeployer(
+        event.params.account.toHex()
+      );
+      const transaction = generateTransaction(event);
+      expressionDeployer.deployTransaction = transaction.id;
 
-    expressionDeployer.save();
+      expressionDeployer.save();
 
-    RainterpreterExpressionDeployerTemplate.create(event.params.implementer);
+      RainterpreterExpressionDeployerTemplate.create(event.params.implementer);
+    }
   }
 }
